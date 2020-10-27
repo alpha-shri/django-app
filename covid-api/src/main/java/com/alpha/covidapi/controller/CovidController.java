9184ba55648c56364b1a169ec5b3a7c621925467
Shrijeet Kumar Panda <shrijeetkumarpanda@gmail.com>
2020-10-27 11:45:41 +0530

    home url

--- covid-api/src/main/java/com/alpha/covidapi/controller/CovidController.java
+++ covid-api/src/main/java/com/alpha/covidapi/controller/CovidController.java
@@ -1,12 +1,16 @@
 package com.alpha.covidapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CovidController {
 
+    @GetMapping("/")
+    @ResponseBody
     public String home(){
-        return "";
+        return "Welcome to Covid-19 Tracker";
     }
     
 }
