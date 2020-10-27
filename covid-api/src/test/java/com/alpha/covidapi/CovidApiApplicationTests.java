8d6731b6a9c13a880be7a1183eebb7a3af32832e
Shrijeet Kumar Panda <shrijeetkumarpanda@gmail.com>
2020-10-27 11:24:37 +0530

    first commit

---
+++ covid-api/src/test/java/com/alpha/covidapi/CovidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.covidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
